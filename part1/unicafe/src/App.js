fc65bc236017b94ed078ed26ac6a74fe22e6ed41
menzi-shazi <93788906+menzi-shazi@users.noreply.github.com>
2023-08-14 18:53:13 +0000

    Exercise 1.6 and 1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,58 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [average,setAverage] = useState(0)
+  const [positive,setPositive] = useState(0)
+  const [total,setTotal] = useState(0)
+
+  const goodButtonClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    const updatedGood = good + 1
+    const updatedTotal = total + 1
+    const avg = (updatedGood + bad * -1)/updatedTotal
+    setAverage(avg)
+    setPositive(updatedGood/updatedTotal)
+  }
+
+  const neutralButtonClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+    const updatedTotal = total + 1
+    const avg = (good + bad * -1)/updatedTotal
+    setAverage(avg)
+    setPositive(good/updatedTotal)
+  }
+
+  const badButtonClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    const updatedBad = bad + 1
+    const updatedTotal = total + 1
+    const avg = (good + updatedBad * -1)/updatedTotal
+    setAverage(avg)
+    setPositive(good/updatedTotal)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick = {goodButtonClick}>good</button>
+      <button onClick = {neutralButtonClick}>neutral</button>
+      <button onClick = {badButtonClick}>bad</button>
+      <h1>Statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {positive}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
